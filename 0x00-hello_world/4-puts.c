4574fe31a89d383d17ecd9fe3f70723fab602837
nanasak <ameyawsamuel@gmail.com>
2023-07-13 21:31:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main - A programe that prints the puts function line
+ * Return:0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
