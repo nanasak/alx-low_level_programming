93416577994a0c51d195286a73021859406812d0
nanasak <ameyawsamuel@gmail.com>
2023-07-14 22:59:22 -0700

    3 combinaitons

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Description: 'prints three digit combinations'
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int a = '0';
+	int b = '0';
+	int c = '0';
+
+	while (a <= '7')
+	{
+		while (b <= '8')
+		{
+			while (c <= '9')
+			{
+				if (a > b && b > c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (!(a == '7' && b == '8' && c == '9'))
+					{
+						putchar(',');
+						putchar(' ')
+					}
+				}
+				c++;
+			}
+			c = '0';
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
