3007239332d4b67bdcda2679a96ab5335b299107
nanasak <ameyawsamuel@gmail.com>
2023-07-20 16:33:15 -0700

    new

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,3 @@
-carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,6 +6,8 @@ carrie@ubuntu:/debugging$ cat main.h
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
 
 void print_remaining_days(int month, int day, int year);
 
