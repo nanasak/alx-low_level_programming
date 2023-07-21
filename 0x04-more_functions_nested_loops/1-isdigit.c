076a867beaf26f263319c8edd55c65da421369f3
nanasak <ameyawsamuel@gmail.com>
2023-07-20 18:34:21 -0700

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: The character to print
+ * Return: On success 1 or 0 on error
+ */
+int _isdigit(int c);
+{
+return (c >= '0' && c <= '9');
+}
