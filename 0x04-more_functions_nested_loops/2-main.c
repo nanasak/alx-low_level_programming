b65d45d98e02f238ca2c98bcf75a227f53f7bcee
nanasak <ameyawsamuel@gmail.com>
2023-07-20 22:57:17 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks for uppercase character
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: On success 0
+ */
+
+int mul (int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
