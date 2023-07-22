a33e7416ab38fd0523cf0da3ea633aa000840647
nanasak <ameyawsamuel@gmail.com>
2023-07-22 06:56:49 -0700

    new diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,15 @@ void print_diagonal(int n)
 {
 		int i, j;
 
-		if (n <= 0)
+			if (n <= 0)
 				_putchar('\n');
-		for (i = 0; i < n; i++)
-		{
-				for (j = 0; j < i; j++)
-				{
-						_putchar(' ');
-				}
+			for (i = 0; i < n; i++)
+			{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 				_putchar('\\');
 				_putchar('\n');
-		}
+			}
 }
