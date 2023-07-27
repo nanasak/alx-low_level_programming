f54374e928ed4923ebbe030379540360c7043abb
nanasak <ameyawsamuel@gmail.com>
2023-07-27 15:29:42 -0700

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - concatenates two strings
@@ -7,7 +8,7 @@
  *
  * Return: void
  */
-char _strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
