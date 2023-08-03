6b35e53d627943d4985bb6fe69a4418e736feb50
nanasak <ameyawsamuel@gmail.com>
2023-08-02 17:13:18 -0700

    fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,6 @@ int _sqrt_recursion(int n)
  * actual_sqrt_recursion - recurses to find the natural square root of a number
  * @n: number to calculate the square root of
  * @i: iterator
-
  * Return: the resulting square root
 */
 int actual_sqrt_recursion(int n, int i)
