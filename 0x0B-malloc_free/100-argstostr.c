f8d84230d61bbe8acaac77f549236e2716fc32a1
nanasak <ameyawsamuel01@gmail.com>
2023-08-12 05:11:46 -0700

    100-argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - convert pramaeters to string
+ * @ac: the count of argument
+ * @av: argument arrays
+ *
+ * Return: pointer to a new string or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int ch = 0, a = 0, b = 0, c = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (a < ac)
+	{
+		while (av[a][b])
+		{
+			ch++;
+			b++;
+		}
+
+		b = 0;
+		a++;
+	}
+
+	s = malloc((sizeof(char) * ch) + ac + 1);
+
+	a = 0;
+	while (av[a])
+	{
+		while (av[a][b])
+		{
+			s[c] = av[a][b];
+			c++;
+			b++;
+		}
+
+		s[c] = '\n';
+
+		b = 0;
+		c++;
+		a++;
+	}
+
+	c++;
+	s[c] = '\0';
+	return (s);
+}
