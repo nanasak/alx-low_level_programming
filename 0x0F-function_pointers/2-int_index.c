5a30d987d24ecd4a710a9c367ee06e47da1307b5
nanasak <ameyawsamuel01@gmail.com>
2023-08-16 18:17:02 -0700

    2-int_inded

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - searches for an integer
+  * @array: the array to be searched
+  * @size: is the number of elements in the array
+  * @cmp: is a pointer to the function to be used to compare values
+  *
+  * Return: -1 if not element matches 
+  */
+int int_inded(int *array, int size, int (*cmp)(int))
+{
+	int x=0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (x < size)
+			{
+				if (cmp(array[x]))
+					return (x);
+
+				x++;
+			}
+		}
+	}
+	return (-1);
+}
